cda72967d09db848769ad7857ea4fa048c9d9207
Ivan Paraskun <ig.paraskun@gmail.com>
2026-01-09 15:41:26 +0000

    dev: bindings

--- pages/Main/jsobjects/JSObject1/JSObject1.js
+++ pages/Main/jsobjects/JSObject1/JSObject1.js
@@ -1,7 +1,181 @@
 export default {
 	state: "Down",
 	config: {
+		"solver": {
+			"iter": 1000,
+			"eval": 1000,
+			"relCost": 1e-10
+		},
+		"cams": {
+			"1m1l": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"1m2l": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"1m1r": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"1m2r": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"2m1l": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"2m2l": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"2m1r": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"2m2r": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"3m1l": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"3m2l": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"3m1r": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"3m2r": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"4m1l": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"4m2l": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"4m1r": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			},
+			"4m2r": {
+				"axes": {
+					"x": {
+						"weight": 1
+					},
+					"z": {
+						"weight": 1
+					}
+				}
+			}
+		}
+	},
 
+	updateX(cam,  weight) {
+		this.config.cams[cam].axes.x.weight = weight;
+	},
+
+	updateZ(cam,  weight) {
+		this.config.cams[cam].axes.z.weight = weight;
 	},
 
 	async up() {
